da88d3e56a3085fecd12d60912f35dc50c26bf5f
Anh Hoang Nguyen <anhnguyen@aaanh.com>
2023-12-14 18:08:21 -0500

    Add legit favicon

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -115,6 +115,8 @@ export default function Home() {
         <title>Reroll.ing</title>
         <meta name="description" content="I bought this domain as a joke." />
         <link rel="icon" href="/logo-color-variant.png" />
+        <meta property="og:image" content="/logo-color-variant.png"></meta>
+        <meta property="og:url" content="https://reroll.ing" />
       </Head>
       <main className={`min-h-screen w-full bg-slate-900 justify-between items-center text-slate-200 flex ${inter.className} flex-col`}>
         <div className="my-4 text-center">
