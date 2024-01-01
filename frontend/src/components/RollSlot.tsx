365166d9f63fc23efc5468f1a729fcba57fd692a
Anh Hoang Nguyen <anhnguyen@aaanh.com>
2024-01-01 13:10:10 -0500

    Improve servant roll slot UI

--- frontend/src/components/RollSlot.tsx
+++ frontend/src/components/RollSlot.tsx
@@ -23,11 +23,11 @@ const RollSlot = ({ roll }: { roll: Roll }) => {
 
   const mapRarityToText = (rarity: number): string => {
     switch(rarity) {
-      case 5: return "SSR"
-      case 4: return "SR"
-      case 3: return "Rare"
-      case 2: return "Common"
-      case 1: return "Normal"
+      case 5: return "★★★★★"
+      case 4: return "★★★★"
+      case 3: return "★★★"
+      case 2: return "★★"
+      case 1: return "★"
       case 0:
       default: return ""
     }
@@ -36,8 +36,7 @@ const RollSlot = ({ roll }: { roll: Roll }) => {
   return <div  className="flex flex-col justify-center items-center m-2 group">
     <Link href={`/servant/${roll.servant?.collectionNo}`} className={`hover:shadow-glow hover:border-blue-500 transition-all ease-in-out duration-300 relative text-center m-2 rounded-lg h-32 w-32 flex items-center justify-center border-2 p-1`.concat(" ", mapRarityToFrameColor(roll.servant?.rarity ?? 0))}>
       {roll.servant?.face && <Image sizes="(max-width: 768px) 100vw" alt={roll.servant.name} fill={true} className="h-24 w-24 rounded-xl p-2" src={roll.servant.face}></Image>}
-      <div className="absolute z-10 bg-black capitalize text-sm opacity-75 bottom-0 right-0">{roll.servant?.name}</div>
-      <div className="absolute z-10 bg-black capitalize text-sm opacity-75 top-0 left-0 rounded-full">{mapRarityToText(roll.servant?.rarity ?? 0)}</div>
+      <div className="absolute z-10 bg-black/50 px-2 capitalize text-sm bottom-1 left-50 rounded-full text-yellow-500">{mapRarityToText(roll.servant?.rarity ?? 0)}</div>
     </Link>
     <div className="w-24 h-12 text-center text-sm">{roll.servant?.name}</div>
   </div>
