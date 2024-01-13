f8886625c80b5f68bb1ac8e93723e221fe8c47ef
Anh Hoang Nguyen <anhnguyen@aaanh.com>
2024-01-13 14:44:09 -0500

    WIP SvCard

---
+++ frontend/src/components/SvCard.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link"
+import Image from "next/image"
+import { FaArrowLeft, FaHome, FaArrowRight } from "react-icons/fa"
+import { type Servant } from "~/types"
+
+const SvCard = (sv: Servant) => {
+  return <main>
+    <section className='sm:border border-white/50 rounded-lg sm:w-1/3 w-full flex sm:flex-row flex-col sm:relative sm:justify-normal sm:items-start justify-center items-center'>
+        <div className='absolute right-2 top-2 rounded-full inline-flex justify-center items-center font-mono text-purple-300 font-bold'>#{sv?.collectionNo}</div>
+        <div className='relative h-32 w-32 m-4'>
+          {<Image sizes="(max-width: 768px) 100vw" alt={sv?.name ?? ""} fill={true} className="h-24 w-24 rounded-xl" src={sv?.face ?? ""}></Image>}
+        </div>
+        <div className='text-center sm:text-left m-4'>
+          <h1 className=''>
+            {sv?.name}{`「${sv?.originalName}」`}
+          </h1>
+          <h2 className='capitalize text-white/50'>
+            {sv?.className}
+          </h2>
+          <h3 className='text-yellow-600'>
+            {
+              sv?.rarity === 5 ? '★★★★★' :
+                sv?.rarity === 4 ? '★★★★' :
+                  sv?.rarity === 3 ? '★★★' :
+                    sv?.rarity === 2 ? '★★' :
+                      sv?.rarity === 1 ? '★' :
+                        'Unknown'
+            }
+          </h3>
+          <p>HP Max: {sv?.hpMax}</p>
+          <p>Attack Max: {sv?.atkMax}</p>
+          <p className='capitalize'>Attribute: {sv?.attribute}</p>
+        </div>
+      </section>
+      <div className='flex justify-between space-x-8 p-4'>
+        <Link href={`/servant/${(sv?.collectionNo ?? 1) - 1}`} className={`hover:text-blue-500 transition-all ease-in-out duration-300 ${sv?.collectionNo == 1 ? 'pointer-events-none text-white/20' : ''}`}>
+          <FaArrowLeft />
+        </Link>
+
+        <Link href={`/`} className={`hover:text-blue-500 transition-all ease-in-out duration-300`}>
+          <FaHome />
+        </Link>
+
+        <Link href={`/servant/${(sv?.collectionNo ?? 1) + 1}`} className={`hover:text-blue-500 transition-all ease-in-out duration-300 ${sv?.collectionNo == 402 ? 'pointer-events-none text-white/20' : ''}`}>
+          <FaArrowRight />
+        </Link>
+      </div>
+  </main>
+}
+
+export default SvCard
---
+++ frontend/src/components/SvModal.tsx
@@ -0,0 +1,12 @@
+import { type Servant } from "~/types"
+
+interface ISvModalProps {
+  servants?: Servant[]
+  isShow: boolean
+}
+
+const SvModal = ({servants, isShow}: ISvModalProps) => {
+  return <div></div>
+}
+
+export default SvModal;
